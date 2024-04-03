657e2f32c3276212199ed045386d3f68ddd0342d
Shubham Sakhare <shubhamsakharegem@gmail.com>
2024-04-03 23:08:34 +0530

    Add UI components

--- Full-Stack/Contact Web App/contactapi/src/main/java/Constant/Constant.java
+++ Full-Stack/Contact Web App/contactapi/src/main/java/Constant/Constant.java
@@ -2,4 +2,5 @@ package Constant;
 
 public class Constant {
     public static final String IMG_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
